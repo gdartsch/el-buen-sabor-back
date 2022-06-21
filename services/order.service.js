f9f33675eb151b5e5dc8f09209c993cae35c18af
sixGdie <sixg.die@gmail.com>
2022-06-21 12:57:42 -0300

    Agregada funcionalidad de de facturas

--- services/order.service.js
+++ services/order.service.js
@@ -9,6 +9,31 @@ const getAll = async () => {
     })
 }
 
+const insertOrder = async (pedido) => {
+  await db.connection
+    .promise()
+    .query(
+      `INSERT INTO pedido (fecha, numero, fk_id_estado, hora_estimada_fin, es_delivery, total, fk_id_domicilio, fk_id_usuario, activo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [
+        pedido.fecha,
+        pedido.numero,
+        pedido.fk_id_estado,
+        pedido.hora_estimada_fin,
+        pedido.es_delivery,
+        pedido.total,
+        pedido.fk_id_domicilio,
+        pedido.fk_id_usuario,
+        pedido.activo,
+      ]
+    )
+  return await db.connection
+    .promise()
+    .query('SELECT LAST_INSERT_ID() AS id')
+    .then(([rows, fields]) => {
+      return rows[0].id
+    })
+}
+
 const getById = async (id) => {
   return await db.connection
     .promise()
@@ -40,5 +65,6 @@ module.exports = {
   getAll,
   getById,
   getByUser,
-  getByDate
+  getByDate,
+  insertOrder,
 }
